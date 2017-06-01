03960d758f7ea7ef988bdb4103d47e504439e649
Sébastien Brisard <sebastien.brisard@m4x.org>
2017-06-01 08:38:52 +0200

    Spider for retrieving table 3.

--- scrapymu/spiders/tables.py
+++ scrapymu/spiders/tables.py
@@ -1,7 +1,34 @@
 import scrapy
 
 BASE_URL = 'http://physics.nist.gov/PhysRefData/XrayMassCoef'
+TABLE1_URL = '/'.join([BASE_URL, 'tab1.html'])
 TABLE3_URL = '/'.join([BASE_URL, 'ElemTab/z{:02d}.html'])
+
+
+def parse_table1_row(row):
+    all_cols = (c.strip() for c in row.css('td::text').extract())
+    cols = [c for c in all_cols if c != '']
+    if cols != []:
+        try:
+            z = int(cols[0])
+            data = {'symbol': cols[1],
+                    'name': cols[2],
+                    'Z/A': cols[3],
+                    'density [g/cm^3]': cols[5]}
+            return z, data
+        except ValueError:
+            return None
+
+
+class Table1Spider(scrapy.Spider):
+    name = 'table1'
+
+    start_urls = [TABLE1_URL]
+
+    def parse(self, response):
+        all_rows = (parse_table1_row(r) for r in response.css('tr'))
+        rows = (r for r in all_rows if r is not None)
+        yield {k: v for k, v in rows}
 
 
 class Table3Spider(scrapy.Spider):
