ba5a68da17614278e39b1a807d650cd6037d2f64
Sébastien Brisard <sebastien.brisard@ifsttar.fr>
2017-06-04 06:27:55 +0200

    Define the `is_float` function to eliminate absorption edge marks.
    This is more robust than skipping a specified amount of white spaces,
    as this amount may vary from element to element.

--- scrapymu/spiders/table3.py
+++ scrapymu/spiders/table3.py
@@ -4,6 +4,14 @@ BASE_URL = 'http://physics.nist.gov/PhysRefData/XrayMassCoef'
 TABLE3_URL = '/'.join([BASE_URL, 'ElemTab/z{:02d}.html'])
 
 
+def is_float(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
 class Table3Spider(scrapy.Spider):
     name = 'table3'
 
@@ -15,6 +23,7 @@ class Table3Spider(scrapy.Spider):
 
         pre = response.css('pre').extract_first()
         lines = pre.splitlines()
-        all_rows = ([x for x in line[3:].split()] for line in lines[6:-1])
+        all_rows = ([x for x in line.split() if is_float(x)]
+                    for line in lines[6:-1])
         rows = [r for r in all_rows if r is not None and r != []]
         yield {z: rows}
